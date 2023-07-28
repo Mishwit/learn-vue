60e6f38d94520c86289768848d0dff95283e8dc6
Mishwit <mgavrilovlife@gmail.com>
2023-07-28 14:18:53 +0200

    123

--- src/vue/vue.js
+++ src/vue/vue.js
@@ -1,6 +1,46 @@
 let app = new Vue({
-  el: "#app",
+  el: "#vue",
   data: {
-    message: "Hello Vue!",
+    counter: 1,
+    // list: ["один", "два", "три"],
+    // users: [
+    //   {
+    //     id: 1,
+    //     name: "Иван",
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Сергей",
+    //   },
+    // ],
+  },
+  // created: function () {
+  //   this.list;
+  // },
+  created: function () {
+    this.counter = 3;
+    this.high();
+  },
+  methods: {
+    high() {
+      this.counter++;
+    },
+    // nameFunction() {
+    //   alert("nameFunction");
+    // },
+    // nameFunction2: function () {
+    //   alert("nameFunction2");
+    // },
   },
 });
+
+// let app = new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Hello Vue!",
+//     status: 1,
+//     isActive: true,
+//     isBtn: true,
+//     width: 15,
+//   },
+// });
